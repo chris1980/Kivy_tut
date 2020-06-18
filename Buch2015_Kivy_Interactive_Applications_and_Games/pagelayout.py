926cb71c3848684bc9e5f890752aa59471fcf1c4
Christian Sannemann <csannemann@gmail.com>
2020-06-18 14:47:22 +0200

    Buch Kivy App PageLayout mit Swipe Funktion

---
+++ Buch2015_Kivy_Interactive_Applications_and_Games/pagelayout.py
@@ -0,0 +1,18 @@
+#Author: 			Christian Sannemann
+#Erstellt am: 		18.06.2020
+#Zuletzt geänder: 	18.06.2020
+
+
+from kivy.app import App
+from kivy.uix.pagelayout import PageLayout
+
+class MyPageLayout(PageLayout):
+	pass
+
+
+class MyPage(App):
+	def build(self):
+		return MyPageLayout()
+
+if __name__ == "__main__":
+	MyPage().run()
