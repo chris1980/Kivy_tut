887c1c49d89fe2617dafca90f86d88a57f30c79a
Christian Sannemann <csannemann@gmail.com>
2020-06-18 19:08:23 +0200

    ComicCreator
    Part 1

---
+++ Buch2015_Kivy_Interactive_Applications_and_Games/comiccreator.py
@@ -0,0 +1,23 @@
+#Author: 			Christian Sannemann
+#Erstellt am:		18.06.2020
+#Geändert am: 		18.06.2020
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.anchorlayout import AnchorLayout
+
+Builder.load_file("toolbox.kv")
+Builder.load_file("zeichenbrett.kv")
+Builder.load_file("optionen.kv")
+Builder.load_file("statusbar.kv")
+
+class ComicCreator(AnchorLayout):
+	pass
+
+class ComicCreatorApp(App):
+	def build(self):
+		return ComicCreator()
+
+if __name__ == "__main__":
+	cc = ComicCreatorApp()
+	cc.run()
