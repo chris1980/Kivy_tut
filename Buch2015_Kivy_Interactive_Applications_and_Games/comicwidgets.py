71d3bded4df6b70df69cbf4e58bab0efa81b5e06
Christian Sannemann <csannemann@gmail.com>
2020-06-20 14:17:46 +0200

    update comicwidgets.kv added functions to comicwidgets.py rahmen in zeichenbrett.kv um den Canvas

--- Buch2015_Kivy_Interactive_Applications_and_Games/comicwidgets.py
+++ Buch2015_Kivy_Interactive_Applications_and_Games/comicwidgets.py
@@ -19,4 +19,26 @@ class BeweglichesWidget(RelativeLayout):
 			self.ix = self.center_x
 			self.iy = self.center_y
 			with self.canvas:
-				self.selected = Line(rectanlge=(0,0,self.width,self.height),dash_offset=2)
+				self.selected = Line(rectangle=(0,0,self.width,self.height), dash_offset=2)
+	def on_touch_move(self, touch):
+		(x,y) = self.parent.to_parent(touch.x,touch.y)
+		if self.selected and self.parent.collide_point(x-self.width/2, y-self.height/2):
+			self.translate(touch.x-self.ix, touch.y - self.iy)
+			return True
+		return super(BeweglichesWidget, self).on_touch_move(touch)
+	def translate(self,x,y):
+		self.center_x = self.ix = self.ix + x
+		self.center_y = self.iy = self.iy + y
+	def on_touch_up(self, touch):
+		if self.selected:
+			self.unselect()
+			return True
+		return super(BeweglichesWidget, self).on_touch_up(touch)
+	def unselect(self):
+		if self.selected:
+			self.canvas.remove(self.selected)
+			self.selected = None
+
+
+class StickMan(BeweglichesWidget):
+	pass
