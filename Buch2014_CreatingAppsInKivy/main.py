540c3821dcc2e7de9410e99b9e7d9681e1484eb1
Christian Sannemann <csannemann@gmail.com>
2020-07-02 23:59:18 +0200

    WetterApp Teil 1

--- Buch2014_CreatingAppsInKivy/main.py
+++ Buch2014_CreatingAppsInKivy/main.py
@@ -1,7 +1,14 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+
+
+class AddLocationForm(BoxLayout):
+	pass
 
 class WeatherApp(App):
-	pass
+	def build(self):
+		return AddLocationForm()
 
 if __name__ == "__main__":
 	wetter = WeatherApp()
